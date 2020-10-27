3576155f7a95e9707e2c2f6b7700edfaedf7c55b
IzzyNety <izzy.nety@gmail.com>
2020-10-27 14:55:49 +0500

    mini trening Vue.js

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos sunt aliquid eius fugiat aspernatur cum animi possimus at numquam natus magnam, maxime reprehenderit reiciendis alias ex amet temporibus sint nobis?</p>
+
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
